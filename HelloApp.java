b862cc5f4f99c1c48e540c487c84cd01c7d1bd60
ikshwak31 <br4915@srmist.edu.in>
2026-03-17 13:13:09 +0530

    UC3: Display Hello with Name or Default World

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,14 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+        String name;
+
+        if(args.length > 0) {
+            name = args[0];
         } else {
-            System.out.println("Hello, World!");
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
